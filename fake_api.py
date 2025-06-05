4be1013eeadaba44043fa175eb31a585d65de02a
khalid A <khalid.akhlafa@gmail.com>
2025-06-05 10:22:26 +0200

    modification de fake_api.py

--- fake_api.py
+++ fake_api.py
@@ -3,16 +3,14 @@ from fastapi.responses import JSONResponse
 from typing import Optional
 import random
 import time
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.getenv("API_TOKEN")
 app = FastAPI()
-
-API_TOKEN = 'CeciEstUnTokenDeTestVousPouvezLeCacherDansDotEnv'
-
 @app.middleware('http')
 async def check_token(request: Request, call_next):
   auth = request.headers.get('Authorization')
-  if not auth or auth != f'Bearer {API_TOKEN}':
-    raise HTTPException(status_code=401, detail='Unauthorized: Invalid or missing token')
   return await call_next(request)
 
 @app.get('/status')
